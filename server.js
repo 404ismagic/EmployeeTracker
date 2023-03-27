8f5ea68c9ede25dbfc107a92091809bf4195ebfb
magic404 <jamesbox3@gmail.com>
2023-03-27 13:26:00 -0700

    New Server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table') 
+const cTable = require('console.table'); 
+
+const dbConn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "tmtrocket",
+    database: "AcsCompany_db"
+});
+
