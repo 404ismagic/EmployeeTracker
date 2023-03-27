8c434f4c28966f34c1969ab0850d5cdee56af93e
magic404 <jamesbox3@gmail.com>
2023-03-27 13:46:42 -0700

    New Seeds.SQLcoDE

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -2,7 +2,9 @@ INSERT INTO department (department_name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
-       ("Legal");
+       ("Legal"),
+       ("Hr"),
+       ("Executive");
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Lead", 3400, 1),
@@ -12,7 +14,9 @@ VALUES ("Sales Lead", 3400, 1),
        ("Account Manager", 233992, 3),
        ("Accountant", 193042, 3),
        ("Legal Team Lead", 931100, 4),
-       ("Lawyer", 230000, 4);
+       ("Lawyer", 230000, 4),
+       ("Hr Director", 9000, 5),
+       ("CEO", 500000, 6);
        
 INSERT into employees (first_name, last_name, role_id, manager_id)
 VALUES  ("Jake", "Freeman", 1, NULL),
@@ -22,4 +26,6 @@ VALUES  ("Jake", "Freeman", 1, NULL),
         ("Kirm", "Natlia", 5, NULL),
         ("Marry", "Jane", 6, 5),
         ("Shroud", "Less", 7, NULL),
-        ("Bare", "Grills", 8, 7);
+        ("Bare", "Grills", 8, 7),
+        ("Tomas", "Hawk", 10, NULL),
+        ("Barry", "Fisher", 9, 9);
